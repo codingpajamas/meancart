e81e96d4ac9423f30ff992f8d257b9a665dcefc5
codingpajamas <nanjiro.ko@gmail.com>
2016-03-04 07:01:16 +0800

    add public directory, add login route in api

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var User = require('../../../models/User');
 var crypto = require('crypto'); 
 var async = require('async'); 
+var passport = require('passport');
 var nodemailer = require('nodemailer'); 
 
 router.post('/register', function(req, res){
-  
+  	
 	User.findOne(
 		{username : req.body.username},
 		function(err, user){
@@ -24,6 +25,16 @@ router.post('/register', function(req, res){
 		}
 	)
 
+});
+
+router.post('/login', function(req, res, next){
+	passport.authenticate('local', function(err, user, info){  
+		if(err || !user){ 
+			res.json({'status':'error', 'message' : info.message})
+		} else { 
+			res.json({'status':'success', 'message' : 'Successfully logged in.'})
+		};
+	})(req, res, next);
 })
 
 module.exports = router;
