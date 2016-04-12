73ab4738a6edc8802cbda888dddc5e64130b0a88
codingpajamas <nanjiro.ko@gmail.com>
2016-04-12 22:12:16 +0800

    fixes and refactors

--- public/app/controllers/customersController.js
+++ public/app/controllers/customersController.js
@@ -1,18 +1,18 @@
 'use strict'
 
 angular.module('starterApp') 
-	.controller('customersController', function($scope, $location, Auth, Post){
+	.controller('customersController', function($scope, $location, Auth){
 		Auth.restrict(); 
 	})
-	.controller('customersViewController', function($scope, $location, $routeParams, Auth, Post){
+	.controller('customersViewController', function($scope, $location, $routeParams, Auth){
 		Auth.restrict(); 
 	})
-	.controller('customersAddController', function($scope, $location, Auth, Post){
+	.controller('customersAddController', function($scope, $location, Auth){
 		Auth.restrict(); 
 	})
-	.controller('customersEditController', function($scope, $location, Auth, Post, $routeParams){
+	.controller('customersEditController', function($scope, $location, Auth, $routeParams){
 		Auth.restrict(); 
 	})
-	.controller('customersDeleteController', function($scope, $location, Auth, Post, $routeParams){
+	.controller('customersDeleteController', function($scope, $location, Auth, $routeParams){
 		Auth.restrict();  
 	})
--- public/app/controllers/ordersController.js
+++ public/app/controllers/ordersController.js
@@ -1,18 +1,18 @@
 'use strict'
 
 angular.module('starterApp') 
-	.controller('ordersController', function($scope, $location, Auth, Post){
+	.controller('ordersController', function($scope, $location, Auth){
 		Auth.restrict(); 
 	})
-	.controller('ordersViewController', function($scope, $location, $routeParams, Auth, Post){
+	.controller('ordersViewController', function($scope, $location, $routeParams, Auth){
 		Auth.restrict(); 
 	})
-	.controller('ordersAddController', function($scope, $location, Auth, Post){
+	.controller('ordersAddController', function($scope, $location, Auth){
 		Auth.restrict(); 
 	})
-	.controller('ordersEditController', function($scope, $location, Auth, Post, $routeParams){
+	.controller('ordersEditController', function($scope, $location, Auth, $routeParams){
 		Auth.restrict(); 
 	})
-	.controller('ordersDeleteController', function($scope, $location, Auth, Post, $routeParams){
+	.controller('ordersDeleteController', function($scope, $location, Auth, $routeParams){
 		Auth.restrict();  
 	})
